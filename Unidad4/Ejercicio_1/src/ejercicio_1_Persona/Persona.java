220cc13177833356bf7ca4d294a63534965a8f3e
Daniel Copete <dcopjim3009g.educaand.es>
2026-01-08 09:52:45 +0100

    2026/1/8 Añadiendo ejercicio 1 de la Unidad 4

---
+++ Unidad4/Ejercicio_1/src/ejercicio_1_Persona/Persona.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio_1_Persona;
+
+/**
+ *
+ * @author 06_1DAW_Alum
+ */
+public class Persona {
+
+    String nombre;
+    byte edad;
+    double estatura;
+   
+    void saludar(){
+       }
+    void cumplir(){
+        
+    }
+    void crecer(){
+    }
+}
+    
