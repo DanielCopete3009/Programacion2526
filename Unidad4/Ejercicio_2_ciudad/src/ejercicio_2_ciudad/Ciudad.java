e11b80d6b30192a7a7d560d1deea5b08a9a5bf82
Daniel Copete <dcopjim3009g.educaand.es>
2026-01-12 13:36:45 +0100

    2026/1/12 Añadiendo ejercicio 2 el operador . de la Unidad 4

--- Unidad4/Ejercicio_2_ciudad/src/ejercicio_2_ciudad/Ciudad.java
+++ Unidad4/Ejercicio_2_ciudad/src/ejercicio_2_ciudad/Ciudad.java
@@ -5,14 +5,19 @@ public class Ciudad {
     public static void main(String[] args) {
 
         // Crear personas
-        Persona daniel = new Persona("Daniel");
-        Persona manuel = new Persona("Manuel");
-        Persona pepe = new Persona("Pepe");
+        Persona daniel = new Persona();
+        Persona manuel = new Persona();
+        Persona pepe = new Persona();
 
         // Tambien se puede poner:
         Persona juan;
         juan = new  Persona();
         
+        // El operador '.' accede  a los miembros de un objeto
+        juan.nombre = "juanje";
+        daniel.nombre = "dani";
+        manuel.nombre = "manuelito";
+        pepe.nombre = "Pepe";
         
         
         
