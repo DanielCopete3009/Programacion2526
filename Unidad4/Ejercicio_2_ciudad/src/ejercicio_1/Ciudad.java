ef3bbe621c853889b675c073768f6b0ad0008b02
Daniel Copete <dcopjim3009g.educaand.es>
2026-01-14 10:56:30 +0100

    2026/1/14 Añadiendo ejercicio 2 el operador . de la Unidad 4

--- Unidad4/Ejercicio_2_ciudad/src/ejercicio_1/Ciudad.java
+++ Unidad4/Ejercicio_2_ciudad/src/ejercicio_1/Ciudad.java
@@ -1,4 +1,5 @@
-package ejercicio_2_ciudad;
+package ejercicio_1;
+
 
 public class Ciudad {
 
@@ -8,6 +9,8 @@ public class Ciudad {
         Persona daniel = new Persona();
         Persona manuel = new Persona();
         Persona pepe = new Persona();
+       
+    
 
         // Otra forma de crear objeto
         Persona juan;
@@ -29,6 +32,27 @@ public class Ciudad {
         pepe.nombre = "Pepe";
         pepe.edad = 30;
         pepe.estatura = 165;
+        
+        //Parte 4 constructores
+        
+        Persona a = new Persona("curro", 20 , 1.60); //Creamos el objeto e lo inicializamos con el constructor
+        Persona b = new Persona("antonio", 24 , 1.88); // Con parámetros
+        
+        Persona paco = new Persona();//Creando dos personas mas 
+        Persona pepee = new Persona();
+        
+        Persona c = new Persona("Alejandro","oñate", 24 , 1.8f);
+        
+        Persona juanje = new Persona("Juan", "Perez", 20, 180f,Persona.Sexo.MASCULINO);
+                
+         System.out.println(juan.nombre);
+         System.out.println(juan.apellido);
+         System.out.println(juan.edad);
+         System.out.println(juan.estatura);
+         System.out.println(juan.sexo);
+        
+        
+        
 
         // Imprimir personas
         System.out.println(daniel);
